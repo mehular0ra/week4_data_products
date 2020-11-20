afd0f2b09665bfafe02a46231c1505a223a50b5c
Mehul Arora <54695911+mehular0ra@users.noreply.github.com>
2020-11-20 18:10:47 +0530

    Update server.R

--- server.R
+++ server.R
@@ -1,15 +1,19 @@
 library(shiny)
 library(datasets)
 
+# asdhgdhggek googel hello i am the google
 DatMPG <- mtcars
+
+#up to you common carup
 DatMPG$am <- factor(DatMPG$am, labels = c("Automatic", "Manual"))
 
+#aldsomtio done gee souuprt
 shinyServer(function(input, output) {
     
     formulaText <- reactive({
         paste("mpg ~", input$variable)
     })
-    
+ # yeepp yeah done   
     formulaTextPoint <- reactive({
         paste("mpg ~", "as.integer(", input$variable, ")")
     })
@@ -17,7 +21,7 @@ shinyServer(function(input, output) {
     fit <- reactive({
         lm(as.formula(formulaTextPoint()), data=DatMPG)
     })
-    
+ # thrid last fingure   
     output$caption <- renderText({
         formulaText()
     })
@@ -27,16 +31,17 @@ shinyServer(function(input, output) {
                 data = DatMPG,
                 outline = input$outliers)
     })
-    
+    # sedond last fingrure
     output$f <- renderPrint({
         summary(fit())
     })
-    
+    # hoperfully lsat
     output$Pompg <- renderPlot({
         with(DatMPG, {
             plot(as.formula(formulaTextPoint()))
             abline(fit(), col=2)
         })
     })
-    
+    # yeah its the last
 })
+# great work almost done cool work
